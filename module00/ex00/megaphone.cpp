fc542a3bb9dd996cb5edec9024ebe2f80c3baa55
Lucas Lethuillier <llethuil@student.42lyon.fr>
2022-06-29 16:41:20 +0200

    ex00 done

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,7 +6,22 @@
 /*   By: llethuil <llethuil@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 11:44:31 by llethuil          #+#    #+#             */
-/*   Updated: 2022/06/29 11:45:13 by llethuil         ###   ########lyon.fr   */
+/*   Updated: 2022/06/29 16:40:41 by llethuil         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	int		i;
+	int		j;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	for (i = 1; i < ac; i ++)
+		for (j = 0; i < av[i][j]; j ++)
+			putchar(toupper(av[i][j]));
+	std::cout << std::endl;
+	return (0);
+}
