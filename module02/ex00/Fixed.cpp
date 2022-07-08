7b123e075968e906a4900c92647ab69d97183886
Lucas Lethuillier <lucas.lethuillier@gmail.com>
2022-07-08 09:54:33 +0200

    modules 00 to 01 done, module02/ex00 done

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: llethuil <lucas.lethuillier@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:55:37 by llethuil          #+#    #+#             */
-/*   Updated: 2022/07/07 18:18:20 by llethuil         ###   ########.fr       */
+/*   Updated: 2022/07/08 09:49:38 by llethuil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,17 +31,17 @@ Fixed   &Fixed::operator=(Fixed const & src)
     std::cout << "Assignation operator called." << std::endl;
     this->_value = src.getRawBits();
     return *this;
-
 }
 
 int     Fixed::getRawBits(void) const
 {
     std::cout << "getRawBits member function called." << std::endl;
-    return (0);
+    return (this->_value);
 }
 
 void    Fixed::setRawBits(int const raw)
 {
+    this->_value = raw;
     return ;
 }
 
--- module02/ex00/Fixed.hpp
+++ module02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: llethuil <lucas.lethuillier@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:54:44 by llethuil          #+#    #+#             */
-/*   Updated: 2022/07/07 18:15:53 by llethuil         ###   ########.fr       */
+/*   Updated: 2022/07/08 09:51:55 by llethuil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,12 @@ class Fixed
     
         Fixed(void);
         Fixed(Fixed const &src);
-        Fixed   &operator=(Fixed const & src);
-        int     getRawBits( void ) const;
-        void    setRawBits( int const raw );
+        Fixed   &operator=(Fixed const &src);
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
         ~Fixed();
     
     private:
         int _value;
-        static int const _decimal_bits;
+        static int const _n_bits_frac_part = 8;
 };
