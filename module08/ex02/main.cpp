67f6d0cc7b0f5af09fabac0362696024c3f1c4a1
Lucas Lethuillier <llethuil@student.42lyon.fr>
2022-09-08 14:23:56 +0200

    module08 cleaned

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: llethuil <llethuil@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 16:13:43 by llethuil          #+#    #+#             */
-/*   Updated: 2022/09/08 14:05:11 by llethuil         ###   ########lyon.fr   */
+/*   Updated: 2022/09/08 14:23:35 by llethuil         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,3 +73,65 @@ int	main(void)
 
 	return 0;
 }
+
+// int main()
+// {
+// 	std::list<int> mstack;
+
+// 	mstack.push_front(5);
+// 	mstack.push_front(17);
+// 	std::cout << mstack.front() << std::endl;
+
+// 	mstack.pop_front();
+// 	std::cout << mstack.size() << std::endl;
+
+// 	mstack.push_back(3);
+// 	mstack.push_back(5);
+// 	mstack.push_back(737);
+// 	mstack.push_back(0);
+
+// 	std::list<int>::iterator it = mstack.begin();
+// 	std::list<int>::iterator ite = mstack.end();
+// 	++it;
+// 	--it;
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+
+// 	std::list<int> s(mstack);
+
+// 	return 0;
+// }
+
+// int main()
+// {
+// 	MutantStack<int> mstack;
+
+// 	mstack.push(5);
+// 	mstack.push(17);
+// 	std::cout << mstack.top() << std::endl;
+
+// 	mstack.pop();
+// 	std::cout << mstack.size() << std::endl;
+
+// 	mstack.push(3);
+// 	mstack.push(5);
+// 	mstack.push(737);
+// 	mstack.push(0);
+
+// 	MutantStack<int>::iterator it = mstack.begin();
+// 	MutantStack<int>::iterator ite = mstack.end();
+// 	++it;
+// 	--it;
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+
+// 	std::stack<int> s(mstack);
+
+// 	return 0;
+// }
